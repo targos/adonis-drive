8aef71ba47befd6bef6e878e035bf1e3ae63c2b8
Michaël Zasso <targos@protonmail.com>
2019-05-29 15:46:35 +0200

    update to @targos/flydrive

--- providers/DriveProvider.js
+++ providers/DriveProvider.js
@@ -7,17 +7,17 @@
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
-*/
+ */
 
 const { ServiceProvider } = require('@adonisjs/fold')
-const FlyDrive = require('@slynova/flydrive')
+const { StorageManager } = require('@targos/flydrive')
 const DriveManager = require('../src/DriveManager')
 
 class DriveProvider extends ServiceProvider {
-  register () {
+  register() {
     this.app.singleton('Adonis/Addons/Drive', (app) => {
       const config = app.use('Adonis/Src/Config').get('drive')
-      const flyDriverInstance = new FlyDrive(config)
+      const flyDriverInstance = new StorageManager(config)
       const { drivers } = DriveManager
 
       /**
