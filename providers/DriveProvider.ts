efde3f36fe0708281a649b6cde9813ee9574f663
Michaël Zasso <targos@protonmail.com>
2020-07-31 11:00:03 +0200

    fix: make correct path for dev mode

--- providers/DriveProvider.ts
+++ providers/DriveProvider.ts
@@ -6,9 +6,10 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+import { isAbsolute } from 'path'
 
 import { IocContract } from '@adonisjs/fold'
-import { StorageManager } from '@slynova/flydrive'
+import { StorageManager, StorageManagerConfig, LocalFileSystemStorageConfig } from '@slynova/flydrive'
 
 /**
  * Provider to bind drive to the container
@@ -21,7 +22,18 @@ export default class DriveProvider {
 	 */
 	public register() {
 		this.container.singleton('Adonis/Addons/Drive', () => {
-			const config = this.container.use('Adonis/Core/Config').get('drive', {})
+			const Application = this.container.use('Adonis/Core/Application')
+			const config: StorageManagerConfig = this.container.use('Adonis/Core/Config').get('drive', {})
+			if (config.disks) {
+				for (const configElement of Object.values(config.disks)) {
+					if (configElement.driver === 'local') {
+						const configElementLocal = configElement.config as LocalFileSystemStorageConfig
+						configElementLocal.root = isAbsolute(configElementLocal.root)
+							? configElementLocal.root
+							: Application.makePathFromCwd(configElementLocal.root)
+					}
+				}
+			}
 			return new StorageManager(config)
 		})
 	}
