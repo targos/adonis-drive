415b8e9049d56cd585c0bbe967930ee2cfa81e10
Michaël Zasso <targos@protonmail.com>
2021-03-23 15:12:59 +0100

    chore: adapt for latest adonis release

--- providers/DriveProvider.ts
+++ providers/DriveProvider.ts
@@ -8,29 +8,28 @@
  */
 import { isAbsolute } from 'path'
 
-import { IocContract } from '@adonisjs/fold'
+import { ApplicationContract } from '@ioc:Adonis/Core/Application'
 import { StorageManager, StorageManagerConfig, LocalFileSystemStorageConfig } from '@slynova/flydrive'
 
 /**
  * Provider to bind drive to the container
  */
 export default class DriveProvider {
-	constructor(protected container: IocContract) {}
+	constructor(protected app: ApplicationContract) {}
 
 	/**
 	 * Register the drive binding
 	 */
 	public register() {
-		this.container.singleton('Adonis/Addons/Drive', () => {
-			const Application = this.container.use('Adonis/Core/Application')
-			const config: StorageManagerConfig = this.container.use('Adonis/Core/Config').get('drive', {})
+		this.app.container.singleton('Adonis/Addons/Drive', () => {
+			const config: StorageManagerConfig = this.app.config.get('drive', {})
 			if (config.disks) {
 				for (const configElement of Object.values(config.disks)) {
 					if (configElement.driver === 'local') {
 						const configElementLocal = configElement.config as LocalFileSystemStorageConfig
 						configElementLocal.root = isAbsolute(configElementLocal.root)
 							? configElementLocal.root
-							: Application.makePathFromCwd(configElementLocal.root)
+							: this.app.makePath(configElementLocal.root)
 					}
 				}
 			}
